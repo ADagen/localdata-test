9bad0b081410a8576bf1c45a0d26fb5d279e2631
Andrey Bodrov <a.bodrov@corp.mail.ru>
2018-08-29 18:20:13 +0300

    add(cookie test)

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 function log(...args) {
-    const timeSerialized = new Date().toLocaleDateString('ru-RU', {
+    const timeSerialized = new Date().toLocaleTimeString('ru-RU', {
         hour12: false,
         hour: '2-digit',
         minute: '2-digit',
@@ -16,4 +16,14 @@ function log(...args) {
 }
 
 log('Start');
+log('======= Cookie');
+log('Before:', document.cookie);
+const testCookie = Math.random().toString(36).substr(2);
+log(`Add new cookie: testCookie=${ testCookie }`);
+document.cookie = `testCookie=${ testCookie }`;
+log('After:', document.cookie);
+log('======= LocalStorage');
+log('N/A');
+log('======= IndexedDB');
+log('N/A');
 log('Finish');
