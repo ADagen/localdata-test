5ae8c04ec0022614aeccfd3d97136afcb519b392
Andrey Bodrov <a.bodrov@corp.mail.ru>
2018-08-30 14:06:05 +0300

    add(js-cookie & localstorage)

--- index.js
+++ index.js
@@ -9,21 +9,43 @@ function log(...args) {
 
     console.log(timeSerialized, ...args);
 
-    const serialized = timeFormatted + ' ' + args.join();
+    const serialized = timeFormatted + ' ' + args.join(' ');
     const logEntry = document.createElement('div');
     logEntry.innerHTML = serialized;
     document.body.appendChild(logEntry);
 }
 
+function logCookies(title) {
+    const content = JSON.stringify(window.Cookies.get(), null, 4);
+    log(title, `<pre>${ content }</pre>`);
+}
+
 log('Start');
-log('======= Cookie');
-log('Before:', document.cookie);
+
+log('=================================== Plain Cookie');
+logCookies('Before:');
 const testCookie = Math.random().toString(36).substr(2);
 log(`Add new cookie: testCookie=${ testCookie }`);
 document.cookie = `testCookie=${ testCookie }`;
-log('After:', document.cookie);
-log('======= LocalStorage');
+logCookies('After:');
+
+log('=================================== Using JS-Cookie');
+const MAX_SAFE_INTEGER = 2 ** 31 - 1;
+const MAX_SAFE_DATE = new Date(MAX_SAFE_INTEGER * 1000);
+logCookies('Before:');
+const testJSCookie = Math.random().toString(36).substr(2);
+log(`Add new cookie: testJSCookie=${ testJSCookie }`);
+window.Cookies.set('testJSCookie', testJSCookie, { expires: MAX_SAFE_DATE });
+logCookies('After:');
+
+log('=================================== LocalStorage');
+const storageKey = 'testLocalStorage';
+const testLocalStorage = Math.random().toString(36).substr(2);
+localStorage.setItem(storageKey, testLocalStorage);
+log(`Add new localStorage item, ${storageKey} = ${ testLocalStorage }`);
+log('After:', localStorage.getItem(storageKey));
+
+log('=================================== IndexedDB');
 log('N/A');
-log('======= IndexedDB');
-log('N/A');
+
 log('Finish');
